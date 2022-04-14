39cf6ac7fc383cf99b4bdab5125efec3a11d4361
Deivz <carlosd_oliveira@hotmail.com>
2022-04-14 01:41:26 -0300

    Inicio da construção do topo

--- src/components/topo/Topo.js
+++ src/components/topo/Topo.js
@@ -3,12 +3,28 @@ import styles from "./topo.module.css";
 function Topo(){
     return(
         <header className={styles.topo}>
-            <h1></h1>
-            <nav className={styles.nav}>
-                <ul className={styles.nav__lista}>
-                    <li></li>
-                </ul>
-            </nav>
+            <div className={styles.topo__container}>
+                <h1></h1>
+                <nav className={styles.nav}>
+                    <ul className={styles.nav__lista}>
+                        <li className={styles.nav__item}>Home</li>
+                        <li className={styles.nav__item}>Sobre</li>
+                        <li className={styles.nav__item}>Competências</li>
+                        <li className={styles.nav__item}>Experiências</li>
+                        <li className={styles.nav__item}>Portfolio</li>
+                        <li className={styles.nav__item}>Contato</li>
+                    </ul>
+                </nav>
+            </div>
+            <section className={styles.secao__principal}>
+                <main className={styles.main__conteudo}>
+                    <h2>Oi, eu sou o <strong>Davi</strong>,</h2>
+                    <h3>e sou desenvolvedor front-end junior!</h3>
+                    <p>Sou engenheiro mecânico em transição de carreira em busca de minha primeira oportunidade.</p>
+                </main>
+                <div>Minha foto aqui</div>
+            </section>
+            
         </header>
     )
 }
