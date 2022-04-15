5f286e5b0b6102cb40e3983284767cf053e2919b
Deivz <carlosd_oliveira@hotmail.com>
2022-04-14 22:37:33 -0300

    Header finalizado

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,52 +1,31 @@
+import { useState } from "react";
 import styles from "./menu.module.css";
 
-// let menuAberto = document.querySelector(".abrir__menu");
-
-// function toggle(event){
-// 	if(menuAberto.classList.contains("change")){
-// 		document.documentElement.classList.remove('menu__ativo');
-// 		menuAberto.classList.remove("change");
-// 		document.documentElement.style.overflow = 'auto';
-// 		document.body.scroll = "yes";
-// 	}else{
-// 		document.documentElement.classList.add('menu__ativo');
-// 		menuAberto.classList.add("change");
-// 		document.documentElement.style.overflow = 'hidden';
-// 		document.body.scroll = "no";
-// 	}
-// }
-
-// menuAberto.addEventListener("click", toggle, false);
-
-// document.documentElement.onclick = function(event) {
-//     if (event.target === document.documentElement) {
-//         document.documentElement.classList.remove('menu-ativo');
-//         menuAberto.classList.remove("change");
-//     }
-// }
-
 function Menu(){
-    return(
-        <>
-            <div>
-                <div className={styles.menu__abrir}>
-                    <div className={styles.bar1}></div>
-                    <div className={styles.bar2}></div>
-                    <div className={styles.bar3}></div>
-                </div>
-            </div>
-            <nav className={styles.nav__menu}>
-                <ul className={styles.nav__lista}>
-                    <li className={styles.nav__item}>Home</li>
-                    <li className={styles.nav__item}>Sobre</li>
-                    <li className={styles.nav__item}>Competências</li>
-                    <li className={styles.nav__item}>Experiências</li>
-                    <li className={styles.nav__item}>Portfolio</li>
-                    <li className={styles.nav__item}>Contato</li>
-                </ul>
-            </nav>
-        </>
-    )
+   const [ativo, setAtivo] = useState(false);
+   function ToggleMode(){
+      setAtivo(!ativo);
+   }
+
+   return (
+         <div className={ativo ? `${styles.menu__ativo} ${styles.menu__abrir}` : `${styles.menu__abrir}`} onClick={ToggleMode}>
+            <div className={ativo ? `${styles.change}` : `${"inativo"}`}>
+            <div className={styles.bar1}></div>
+            <div className={styles.bar2}></div>
+            <div className={styles.bar3}></div>
+         </div>
+         <nav className={styles.nav__menu}>
+            <ul className={styles.nav__lista}>
+               <li className={styles.nav__item}>Home</li>
+               <li className={styles.nav__item}><a href="#">Sobre</a></li>
+               <li className={styles.nav__item}><a href="#">Competências</a></li>
+               <li className={styles.nav__item}><a href="#">Experiências</a></li>
+               <li className={styles.nav__item}><a href="#">Portfolio</a></li>
+               <li className={styles.nav__item}><a href="#">Contato</a></li>
+            </ul>
+         </nav>
+      </div>
+   )
 }
 
 export default Menu
--- src/components/topo/Topo.js
+++ src/components/topo/Topo.js
@@ -1,24 +1,34 @@
 import styles from "./topo.module.css";
 import Menu from "../menu/Menu";
+import fotoAutor from "../../assets/portfoto.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 
-function Topo(){
-    return(
-        <header className={styles.topo}>
-            <div className={styles.topo__container}>
-                <h1></h1>
-                <Menu />
-            </div>
-            <section className={styles.secao__principal}>
-                <main className={styles.main__conteudo}>
-                    <h2>Oi, eu sou o <strong>Davi</strong>,</h2>
-                    <h3>e sou desenvolvedor front-end junior!</h3>
-                    <p>Sou engenheiro mecânico em transição de carreira em busca de minha primeira oportunidade.</p>
-                </main>
-                <div>Minha foto aqui</div>
-            </section>
-            
-        </header>
-    )
+function Topo() {
+   return (
+      <header className={styles.topo}>
+         <div className={styles.topo__container}>
+            <h1></h1>
+            <Menu />
+         </div>
+         <section className={styles.secao__principal}>
+            <main className={styles.main__conteudo}>
+               <h2>Oi, eu sou <strong>Davi</strong>,</h2>
+               <h3>e sou desenvolvedor front-end junior!</h3>
+               <p>Sou engenheiro mecânico em transição de carreira em busca de minha primeira oportunidade.</p>
+               <div className={styles.icones}>
+                  <span><a href="https://www.linkedin.com/in/carlos-davi-oliveira/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a></span>
+                  <span><a href="https://github.com/Deivz" target="_blank" rel="noopener noreferrer"><FaGithub /></a></span>
+                  <span><a href="https://www.instagram.com/deivzz/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a></span>
+                  <span><a href="https://www.facebook.com/carlosd.oliveira" target="_blank" rel="noopener noreferrer"><FaFacebook /></a></span>
+               </div>
+            </main>
+            <img src={fotoAutor} alt="Foto do autor da página" className={styles.foto}></img>
+         </section>
+      </header>
+   )
 }
 
 export default Topo
