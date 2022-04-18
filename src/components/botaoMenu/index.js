ea4f6c942d6fe974c90a13f58f0149b94faeb3db
Deivz <carlosd_oliveira@hotmail.com>
2022-04-18 19:26:56 -0300

    Finalização da refatoração da navegação, topo e home.

--- src/components/botaoMenu/index.js
+++ src/components/botaoMenu/index.js
@@ -2,11 +2,15 @@ import styles from "./botaoMenu.module.css"
 
 function BotaoMenu({evento, ativo}){
    return(
-      <div className={ativo ? `${styles.botao__menu} ${styles.change}` : `${styles.botao__menu}`} onClick={evento}>
-         <div className={styles.bar1}></div>
-         <div className={styles.bar2}></div>
-         <div className={styles.bar3}></div>
-      </div>
+      <section className={styles.menu}>
+         <div className={styles.botao__menu}>
+            <div className={ativo ? `${styles.change}` : ``} onClick={evento}>
+               <div className={styles.bar1}></div>
+               <div className={styles.bar2}></div>
+               <div className={styles.bar3}></div>
+            </div>
+         </div>
+      </section>
    )
 }
 
--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -3,14 +3,16 @@ import {links} from "./links"
 
 function Menu({ativo}){
    return (
-      <div className={ativo ? `${styles.container} ${styles.menu__ativo}` : `${styles.container}`}>
-         <nav className={styles.nav__menu}>
-            <ul className={styles.nav__lista}>
-               {links.map((link)=>{
-                  return <li className={styles.nav__item}><a href={link.url} key={link.id}>{link.texto}</a></li>
-               })}
-            </ul>
-         </nav>
+      <div className={styles.navegacao}>
+         <div className={ativo ? `${styles.container} ${styles.menu__ativo}` : `${styles.container}`}>
+            <nav className={styles.nav__menu}>
+               <ul className={styles.nav__lista}>
+                  {links.map((link)=>{
+                     return <li className={styles.nav__item}><a href={link.url} key={link.id}>{link.texto}</a></li>
+                  })}
+               </ul>
+            </nav>
+         </div>
       </div>
    )
 }
