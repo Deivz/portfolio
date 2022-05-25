b6e0ddd077af53c0b862c1175fef8288b3135d20
Deivz <carlosd_oliveira@hotmail.com>
2022-05-25 00:27:27 -0300

    Validação do formulário implementada, falta enviar email

--- src/components/formulario/index.js
+++ src/components/formulario/index.js
@@ -4,12 +4,15 @@ import {FaEnvelope} from "react-icons/fa"
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
-import { Box, TextField } from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
+import mensagensErro from "../../utilities/errors.json";
+import SendIcon from '@mui/icons-material/Send';
+import regExNome from "../../utilities/regexValidation.js"
 
 export default function Formulario() {
 
    const validacao = yup.object().shape({
-      nome: yup.string().min(2).max(50).matches().required(),
+      nome: yup.string().min(2).max(50).matches(regExNome).required(),
       assunto: yup.string().min(2).max(50).required(),
       email: yup.string().email().max(50).required(),
       mensagem: yup.string().min(2).max(200).required()
@@ -27,15 +30,61 @@ export default function Formulario() {
     <section className={styles.secao__formulario} id="contato">
       <h2 className={styles.titulo}><FaEnvelope style={{paddingBottom: 6}}/>Contate-me</h2>
       <form name="form" className={styles.form} id="form" onSubmit={handleSubmit(enviar)}>
-         <Box>
+         <Box className={styles.box}>
             <TextField
                name="nome"
+               type="text"
+               error={errors?.nome ? "Erro" : ""}
+               helperText={errors?.nome ? mensagensErro["nome"][errors.nome.type] : ""}
                {...register("nome")}
                id="nome"
                label="Nome"
                variant="outlined"
+               className={styles.campo}
             />
          </Box>
+         <Box className={styles.box}>
+            <TextField
+               name="assunto"
+               type="text"
+               error={errors?.assunto ? "Erro" : ""}
+               helperText={errors?.assunto ? mensagensErro["assunto"][errors.assunto.type] : ""}
+               {...register("assunto")}
+               id="assunto"
+               label="Assunto"
+               variant="outlined"
+               className={styles.campo}
+            />
+         </Box>
+         <Box className={styles.box}>
+            <TextField
+               name="email"
+               type="email"
+               error={errors?.email ? "Erro" : ""}
+               helperText={errors?.email ? mensagensErro["email"][errors.email.type] : ""}
+               {...register("email")}
+               id="email"
+               label="E-mail"
+               variant="outlined"
+               className={styles.campo}
+            />
+         </Box>
+         <Box className={styles.box}>
+            <TextField
+               name="mensagem"
+               type="text"
+               error={errors?.mensagem ? "Erro" : ""}
+               helperText={errors?.mensagem ? mensagensErro["mensagem"][errors.mensagem.type] : ""}
+               {...register("mensagem")}
+               id="mensagem"
+               label="Mensagem"
+               variant="outlined"
+               className={styles.campo}
+            />
+         </Box>
+         <Button color="success" type="submit" variant="contained" endIcon={<SendIcon />}>
+            Send
+         </Button>
       </form>
     </section>
   )
